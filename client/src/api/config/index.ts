813f6ac1290583ac804f777693e82189b57b2be4
Muhammad Usman <Musmant11@gmail.com>
2023-09-26 17:08:10 +0500

    Update client/ for vercel hosting

--- client/src/api/config/index.ts
+++ client/src/api/config/index.ts
@@ -1,14 +1,16 @@
-const BACKEND_BASE_URL = import.meta.env.API_BASE_URL as string;
-const NEWS_API_KEY = import.meta.env.NEWS_API_KEY as string;
-
+const BACKEND_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+const NEWS_API_KEY = import.meta.env.VITE_NEWS_API_KEY;
+console.log("news api key: ", NEWS_API_KEY);
 let homeUri: string;
 
 const development: boolean =
-  !import.meta.env.NODE_ENV || import.meta.env.NODE_ENV === "development";
+  !import.meta.env.VITE_NODE_ENV ||
+  import.meta.env.DEV ||
+  import.meta.env.VITE_NODE_ENV === "development";
 if (development) {
   homeUri = "http://localhost:3000";
 } else {
-  homeUri = BACKEND_BASE_URL as string;
+  homeUri = BACKEND_BASE_URL;
 }
 
 export { homeUri, NEWS_API_KEY };
